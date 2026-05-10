ff9b82656cc4866540c36d8bc04ef8055f98281c
franci2003 <s312541@studenti.polito.it>
2026-05-10 16:40:22 +0200

    t

--- eomt/evalEoMTTemperature.py
+++ eomt/evalEoMTTemperature.py
@@ -82,14 +82,15 @@ def eomt_to_pixel_logits(mask_logits_per_layer, class_logits_per_layer):
     )
 
     mask_prob = torch.sigmoid(mask_logits) # quanto la query copre il pixel
-    class_prob = torch.softmax(class_logits, dim=-1) # probabilità che la query appartenga ad una classe
 
-    class_prob = class_prob[:, :, :-1] # scarta la classe no object perché vogliamo una mappa delle classi reali
+    # logit delle classi senza softmax
+    # scartiamo l'ultima colonna che corrisponde a no-object
+    class_logits_raw = class_logits[:, :, :-1]
 
-    pixel_scores = torch.einsum("bqc,bqhw->bchw", class_prob, mask_prob)
-    pixel_scores = pixel_scores.squeeze(0)
+    # combiniamo i logit con le maschere
+    pixel_logits = torch.einsum("bqc,bqhw->bchw", class_logits_raw, mask_prob)
 
-    return pixel_scores
+    return pixel_logits.squeeze(0)
 
 
 def main():
