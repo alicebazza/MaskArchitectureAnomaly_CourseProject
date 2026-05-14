e73491bb28e3baa91faf204e1cc23791f93d817e
alicebazza <s309959@studenti.polito.it>
2026-05-14 16:26:05 +0200

    miou

--- eval/iouEval.py
+++ eval/iouEval.py
@@ -20,57 +20,58 @@ class iouEval:
         self.fp = torch.zeros(classes).double()
         self.fn = torch.zeros(classes).double()        
 
-    def addBatch(self, x, y):   # x = predizioni del modello, y = etichette reali (ground truth)
-    # input ---> [Batch, Classi, Altezza, Larghezza]
+    def addBatch(self, x, y):
+        # x = predizioni del modello, y = etichette reali (ground truth)
+        # input ---> [Batch, Classi, Altezza, Larghezza]
 
-    if (x.is_cuda or y.is_cuda):
-        x = x.cuda()
-        y = y.cuda()
+        if (x.is_cuda or y.is_cuda):
+            x = x.cuda()
+            y = y.cuda()
 
-    # one hot encoding
-    if (x.size(1) == 1):
-        x_onehot = torch.zeros(x.size(0), self.nClasses, x.size(2), x.size(3))
-        if x.is_cuda:
-            x_onehot = x_onehot.cuda()
-        # riempie il tensore di zeri mettendo un "1" nella posizione corrispondente alla classe predetta
-        x_onehot.scatter_(1, x, 1).float()
-    else:
-        # se è già in formato one-hot lo converte in numeri decimali
-        x_onehot = x.float()
+        # one hot encoding
+        if (x.size(1) == 1):
+            x_onehot = torch.zeros(x.size(0), self.nClasses, x.size(2), x.size(3))
+            if x.is_cuda:
+                x_onehot = x_onehot.cuda()
+            # riempie il tensore di zeri mettendo un "1" nella posizione corrispondente alla classe predetta
+            x_onehot.scatter_(1, x, 1).float()
+        else:
+            # se è già in formato one-hot lo converte in numeri decimali
+            x_onehot = x.float()
 
-    if (y.size(1) == 1):
-        y_onehot = torch.zeros(y.size(0), self.nClasses, y.size(2), y.size(3))
-        if y.is_cuda:
-            y_onehot = y_onehot.cuda()
-        y_onehot.scatter_(1, y, 1).float()
-    else:
-        y_onehot = y.float()
+        if (y.size(1) == 1):
+            y_onehot = torch.zeros(y.size(0), self.nClasses, y.size(2), y.size(3))
+            if y.is_cuda:
+                y_onehot = y_onehot.cuda()
+            y_onehot.scatter_(1, y, 1).float()
+        else:
+            y_onehot = y.float()
 
-    # gestione dell'indice da ignorare
-    if (self.ignoreIndex != -1):
-        ignores = y_onehot[:,self.ignoreIndex].unsqueeze(1)
-        # rimuove la classe da ignorare da entrambi i tensori
-        x_onehot = x_onehot[:, :self.ignoreIndex]
-        y_onehot = y_onehot[:, :self.ignoreIndex]
-    else:
-        ignores=0
+        # gestione dell'indice da ignorare
+        if (self.ignoreIndex != -1):
+            ignores = y_onehot[:,self.ignoreIndex].unsqueeze(1)
+            # rimuove la classe da ignorare da entrambi i tensori
+            x_onehot = x_onehot[:, :self.ignoreIndex]
+            y_onehot = y_onehot[:, :self.ignoreIndex]
+        else:
+            ignores=0
 
-        #print(type(x_onehot))
-        #print(type(y_onehot))
-        #print(x_onehot.size())
-        #print(y_onehot.size())
+            #print(type(x_onehot))
+            #print(type(y_onehot))
+            #print(x_onehot.size())
+            #print(y_onehot.size())
 
-        tpmult = x_onehot * y_onehot    # true positive
-        tp = torch.sum(torch.sum(torch.sum(tpmult, dim=0, keepdim=True), dim=2, keepdim=True), dim=3, keepdim=True).squeeze()
-        fpmult = x_onehot * (1-y_onehot-ignores) # false positive
-        # sommiamo i pixel per ottenere il totale dei falsi positivi per ogni classe
-        fp = torch.sum(torch.sum(torch.sum(fpmult, dim=0, keepdim=True), dim=2, keepdim=True), dim=3, keepdim=True).squeeze()
-        fnmult = (1-x_onehot) * (y_onehot) # false negative
-        fn = torch.sum(torch.sum(torch.sum(fnmult, dim=0, keepdim=True), dim=2, keepdim=True), dim=3, keepdim=True).squeeze()
+            tpmult = x_onehot * y_onehot    # true positive
+            tp = torch.sum(torch.sum(torch.sum(tpmult, dim=0, keepdim=True), dim=2, keepdim=True), dim=3, keepdim=True).squeeze()
+            fpmult = x_onehot * (1-y_onehot-ignores) # false positive
+            # sommiamo i pixel per ottenere il totale dei falsi positivi per ogni classe
+            fp = torch.sum(torch.sum(torch.sum(fpmult, dim=0, keepdim=True), dim=2, keepdim=True), dim=3, keepdim=True).squeeze()
+            fnmult = (1-x_onehot) * (y_onehot) # false negative
+            fn = torch.sum(torch.sum(torch.sum(fnmult, dim=0, keepdim=True), dim=2, keepdim=True), dim=3, keepdim=True).squeeze()
 
-        self.tp += tp.double().cpu()
-        self.fp += fp.double().cpu()
-        self.fn += fn.double().cpu()
+            self.tp += tp.double().cpu()
+            self.fp += fp.double().cpu()
+            self.fn += fn.double().cpu()
 
     def getIoU(self):
     num = self.tp  # numeratore: i pixel correttamente identificati
