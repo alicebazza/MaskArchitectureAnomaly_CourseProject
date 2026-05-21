95e150df18f704c3cb0711e0495cd7417ed39593
alicebazza <s309959@studenti.polito.it>
2026-05-21 21:38:55 +0200

    fine

--- eomt/datasets/cityscapes_semantic.py
+++ eomt/datasets/cityscapes_semantic.py
@@ -9,11 +9,11 @@ from typing import Union
 from torch.utils.data import DataLoader
 from torchvision.datasets import Cityscapes
 
-from datasets.lightning_data_module import LightningDataModule
-from datasets.dataset import Dataset
-from datasets.transforms import Transforms
-from datasets.coco_ood_paster import CocoOODPaster
-from datasets.ood_wrapper import OODDatasetWrapper
+from eomt.datasets.lightning_data_module import LightningDataModule
+from eomt.datasets.dataset import Dataset
+from eomt.datasets.transforms import Transforms
+from eomt.datasets.coco_ood_paster import CocoOODPaster
+from eomt.datasets.ood_wrapper import OODDatasetWrapper
 
 
 class CityscapesSemantic(LightningDataModule):
